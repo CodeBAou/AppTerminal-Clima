1d3597944e3f079321791f82f5ec7834df030177
CodeBAou <borisafou@gmail.com>
2022-08-14 22:31:38 +0200

    v1.0

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const Busquedas = require('./models/busquedas');
 const Busqueda = require('./models/busquedas');
 
 
-
-
 const main = async() => {
     
     const busquedas = new Busquedas();
@@ -25,26 +23,40 @@ const main = async() => {
             case '1':
                 //Mostrar mensaje
                 const termino = await leerInput( 'Ciudad: ' );
-                const lugares = await busquedas.ciudad( termino );
-                const id = await listarLugares(lugares);
-                const lugarSel = lugares.find( l => l.id === id ); // find devuelve el primer elemento que cumpla la condicion
-                
-
+  
                 //Buscar los lugares
+                const lugares = await busquedas.ciudad( termino );
 
                 //Seleccionar el lugar
+                const id = await listarLugares(lugares);
+                if( id === '0' ) continue;
+                const lugarSel = lugares.find( l => l.id === id ); // find devuelve el primer elemento que cumpla la condicion
+                
+                //Guardar en db 
+                busquedas.agregarHistorial( lugarSel.nombre );
 
                 //Clima
+                const clima = await busquedas.climaLugar( lugarSel.lat, lugarSel.lng );
+                console.log(clima);
 
                 //Mostrar resultados
-
                 console.log('\n Informacion de la ciudad\n'.green);
                 console.log('Ciudad:',lugarSel.nombre);
                 console.log('Lat:',lugarSel.lat);
                 console.log('Lng:',lugarSel.lng);
-                console.log('Temperatura:',);
-                console.log('Minima:',);
-                console.log('Maxima:',);
+                console.log('Temperatura:',clima.temp);
+                console.log('Minima:',clima.min);
+                console.log('Maxima:', clima.max);
+                console.log('Descripcion Clima:',clima.desc);
+                break;
+
+            case '2':
+             
+                busquedas.historialCapitalizado.forEach( ( lugar, i ) => {
+                        const idx = `${i+1}.`.green;
+                        console.log( `${idx} ${lugar}` );
+                    }
+                );
                 break;
         }
 
--- models/busquedas.js
+++ models/busquedas.js
@@ -1,18 +1,43 @@
+const fs = require('fs');
 const axios = require('axios');
 
 class Busquedas{
 
-    historial = ['Tergucigalpa', 'Madrid', 'San Jose'];
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor(){
         //TODO: leer DB si existe
+        try{
+            this.leerDB();
+        }catch(err){
+            console.log(err);
+        }
+        
+    }
+
+    get historialCapitalizado(){
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras     = palabras.map( p=> p[0].toUpperCase() + p.substring(1) );
+            return palabras.join(' ');
+        });
+        return this.historial;
     }
 
     get paramsMapbox(){
         return {
-            'access_token' : 'pk.eyJ1IjoiY29kZTQ0NTYiLCJhIjoiY2w2cW91ZXZwMGNlczNrbjJyZHRwa2ZsayJ9.EZ41m0BTClxr_M88uqracA',
+            'access_token' : process.env.MAPBOX_KEY,
             'limit': 5,
             'languaje': 'es'
+        }
+    }
+
+    get paramsOpenweathermap(){
+        return{
+            appid:'0456319b5c72a92886f7b8a52de3bbf1',
+            units:'metric',
+            lang:'es'
         }
     }
 
@@ -43,6 +68,62 @@ class Busquedas{
         }catch(error){
             return [];
         }
+    }
+
+    async climaLugar( lat, lon ){
+
+        try{
+            //Instancia axios.create()
+            const instance = axios.create(
+                {
+                    baseURL:`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`,
+                    params:this.paramsOpenweathermap
+                }
+            )
+
+            const resp = await instance.get();
+         
+            //resp.data
+            return{
+                desc:resp.data.weather[0].description,
+                min:resp.data.main.temp_min,
+                max:resp.data.main.temp_max,
+                temp:resp.data.main.temp
+            }
+
+        }catch( error ){
+            console.log(error);
+        }
+    }
+
+    agregarHistorial( lugar = '' ){
+        
+        if( this.historial.includes( lugar.toLocaleLowerCase() ) ){
+            return;//ya existe no se guarda
+        }
+
+        //prevenir duplicados
+        this.historial.unshift( lugar.toLocaleLowerCase() );
+
+        // Grabar en DB
+        this.guardarDB();
+    }
+    
+    guardarDB(){
+
+        const payload = {
+            historial: this.historial
+        }
+
+        fs.writeFileSync( this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB(){
+
+        if(!fs.existsSync(this.dbPath) ) return;       
+        const info     = fs.readFileSync( this.dbPath, { encoding: 'utf-8'} );
+        const data     = JSON.parse( info );
+        this.historial = data.historial;
     }
 }
 
